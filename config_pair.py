5d98a6e7a0d65f70aa174ecf2c929fe67af7c6fd
081061 <enplus@daishin.com>
2017-08-15 14:31:23 +0900

    First commit

---
+++ config_pair.py
@@ -0,0 +1,14 @@
+from config import *
+
+USE_MULTITHREADED = False
+# EXCHANGES = ['CRYPTO-TRADE', 'BTCE', 'COINONE', 'CRYPTSY']
+EXCHANGES = ['COINONE', 'KORBIT']
+# 'COINEX' is down, will be back up soon
+# 'BTER' also doing weird shit...
+# 'VIRCUREX' is insolvent...
+#PAIRS = [("LTC","BTC"), ("DOGE", "BTC"), ("PPC","BTC"), ("NXT","BTC"), ("WDC","BTC"),
+#       ("DOGE","LTC"), ("PPC","LTC")]
+
+#EXCHANGES = ['BTER']
+PAIRS = [("LTC","BTC"), ("DOGE","BTC")]
+MODE = 'BACKTEST'
